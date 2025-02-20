9ef0602d5a8503e9111a961b5b2663a6ee114afb
Tristan Ernst <60721601+Tanoflame@users.noreply.github.com>
2025-02-20 18:10:31 -0500

    Initial Commit

---
+++ src/main/java/net/tanoflame/bananaalert/BananaAlert.java
@@ -0,0 +1,15 @@
+package net.tanoflame.bananaalert;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BananaAlert implements ClientModInitializer {
+	public static final String MOD_ID = "banana-alert";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+	}
+}
