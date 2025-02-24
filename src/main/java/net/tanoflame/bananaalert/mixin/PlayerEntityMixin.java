4b3530902f66c900566bd56a4cf144dde8312b2b
Tristan Ernst <60721601+Tanoflame@users.noreply.github.com>
2025-02-24 16:28:58 -0500

    feat: nametag above head now renders list color

--- src/main/java/net/tanoflame/bananaalert/mixin/PlayerEntityMixin.java
+++ src/main/java/net/tanoflame/bananaalert/mixin/PlayerEntityMixin.java
@@ -1,18 +1,35 @@
 package net.tanoflame.bananaalert.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Util;
+import net.tanoflame.bananaalert.PlayerEntry;
+import net.tanoflame.bananaalert.PlayerList;
+import net.tanoflame.bananaalert.PlayerListManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.UUID;
+
 @Mixin(net.minecraft.entity.player.PlayerEntity.class)
-public class PlayerEntityMixin {
+public abstract class PlayerEntityMixin {
+    @Shadow public abstract GameProfile getGameProfile();
+
     @Inject(method = "getDisplayName", at = @At("RETURN"))
     public void getDisplayName(CallbackInfoReturnable<Text> cir, @Local MutableText mutableText) {
-        mutableText.formatted(Formatting.BLUE);
+        UUID uuid = this.getGameProfile().getId();
+
+        PlayerEntry entry = PlayerListManager.getPlayerEntry(uuid);
+        if (entry != null && entry.getPlayerListId() != Util.NIL_UUID) {
+            PlayerList list = PlayerListManager.getList(entry.getPlayerListId());
+
+            mutableText.formatted(list.getColor());
+        }
     }
 }
